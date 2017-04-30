e0b31b0fdf5e20faf6b6b56e0aff4f2e1af1d1e7
nekomeo <elleti@Elles-MacBook-Air.local>
2017-04-30 12:15:05 -0700

    Initial Commit

---
+++ Assignment 3: Toyotas/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Assignment 3: Toyotas
+//
+//  Created by Elle Ti on 2017-04-30.
+//  Copyright © 2017 Elle Ti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
